77edea2c6f5f02a04ab97d2ede4ffdcdf89918a6
Joel Jorge Nunes Vázquez <jnunesvazquez@danielcastelao.org>
2021-02-23 11:49:12 +0100

    Actualizar ObjetoConsola con constructor

--- src/com/libreria/ObjetoConsola.java
+++ src/com/libreria/ObjetoConsola.java
@@ -4,6 +4,12 @@ import javax.swing.*;
 
 public class ObjetoConsola {
 
+    private String msg;
+
+    public ObjetoConsola (String msg){
+        this.msg = msg;
+    }
+
     public void imprimirPorConsola(){
         String elemento = JOptionPane.showInputDialog("Introducir el mensaje a mostrar");
         JOptionPane.showMessageDialog(null, elemento);
