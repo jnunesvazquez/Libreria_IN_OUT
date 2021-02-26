757c16ac8c1ea22735d804bf5fcd24debf660ff1
Joel Jorge Nunes Vázquez <jnunesvazquez@danielcastelao.org>
2021-02-26 09:48:17 +0100

    Actualizando la FactoriaDeSalidas

--- src/com/libreria/FactoriaDeSalidas.java
+++ src/com/libreria/FactoriaDeSalidas.java
@@ -3,6 +3,7 @@ package com.libreria;
 public class FactoriaDeSalidas {
     public static final int CONSOLA = 1;
     public static final int VENTANA = 2;
+    public static final int IMPRESORA = 3;
 
     public static ISalida getProducto(int type, String msg){
         switch (type){
@@ -10,6 +11,8 @@ public class FactoriaDeSalidas {
                 return new ObjetoConsola(msg);
             case VENTANA:
                 return new ObjetoPantalla(msg);
+            case IMPRESORA:
+                return new Impresora(msg);
             default:
                 return null;
         }
