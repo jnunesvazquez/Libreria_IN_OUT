ba6467829211c6cbc493679b9afa5fc10bde4990
Joel Jorge Nunes Vázquez <jnunesvazquez@danielcastelao.org>
2021-02-26 08:49:08 +0100

    Actualizando la clase ObjetoPantalla

--- src/com/libreria/ObjetoPantalla.java
+++ src/com/libreria/ObjetoPantalla.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 
 public class ObjetoPantalla {
 
+    private String msg;
+
+    public ObjetoPantalla (String msg){
+        this.msg = msg;
+    }
+
     public void imprimirPorPantalla(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Introducir el mensaje a mostrar");
